e14c9cbe595a445dd383f1a8e516b42c12682350
Flynthia <165576650+Flynthia@users.noreply.github.com>
2024-04-02 10:01:35 +0700

    Update DAY3_practice.sql

--- DAY3_practice.sql
+++ DAY3_practice.sql
@@ -30,6 +30,34 @@ SELECT name FROM Employee
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id
 
+-- EX9. Leetcode
+SELECT product_id FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
+
+-- EX10.
+SELECT name FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2
+
+-- EX11.
+SELECT name, population, area FROM World
+WHERE area >= 3000000 OR population >= 25000000
+
+-- EX12.
+SELECT DISTINCT author_id AS id FROM Views
+WHERE viewer_id = author_id
+ORDER BY author_id 
+
+-- EX13.
+SELECT DISTINCT part FROM parts_assembly
+WHERE finish_date IS NULL
+
+-- EX 14.
+select * from lyft_drivers
+where yearly_salary <= 30000 OR yearly_salary >= 70000
+
+-- EX 15.
+select * from uber_advertising
+where money_spent > 100000
 
 
 
