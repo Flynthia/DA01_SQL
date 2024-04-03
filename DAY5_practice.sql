3b452482e613585c3eb157c1110c81b5e9fc9951
Flynthia <165576650+Flynthia@users.noreply.github.com>
2024-04-03 19:27:20 +0700

    Update DAY5_practice.sql

--- DAY5_practice.sql
+++ DAY5_practice.sql
@@ -18,3 +18,44 @@ HAVING COUNT(skill)=3
 ORDER BY candidate_id
 
 -- EX6
+SELECT user_id, 
+DATE(MAX(post_date)) - DATE(MIN(post_date)) as days_between
+FROM posts
+WHERE post_date >= '2021-01-01' AND post_date < '2022-01-01'
+GROUP BY user_id
+HAVING COUNT(post_id) >=2
+
+-- EX7
+SELECT card_name, MAX(issued_amount)-MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC
+
+-- EX8
+SELECT manufacturer, COUNT (drug) AS drug_count, SUM(total_sales - cogs) AS total_loss
+FROM pharmacy_sales
+WHERE total_sales - cogs <=0
+GROUP BY manufacturer
+ORDER BY total_loss DESC
+
+-- EX9
+SELECT * FROM Cinema
+WHERE id%2=1 AND description != 'boring'
+ORDER BY rating DESC
+
+-- EX10
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id
+
+-- EX11
+SELECT user_id, COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id 
+ORDER BY user_id
+
+-- EX12
+SELECT class FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5
+
